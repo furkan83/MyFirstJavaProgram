5ac535e4f1668603ddfc00c05fb7e981dd096d78
furkan83 <118264064+furkan83@users.noreply.github.com>
2023-10-22 18:05:29 +0300

    create no1.java

---
+++ no1.java
@@ -0,0 +1,12 @@
+public class HideAndSeek {
+  public static void main(String[] args) {
+    System.out.println("Let's play hide and seek.");
+    System.out.print("Three...");
+    System.out.print("Two...");
+    System.out.println("One...");
+    System.out.println("Ready or not, here I come!");
+//This is a comment.
+/*This is another way to comment.
+This method is used for multiple lines.*/
+  }
+}

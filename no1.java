4a6e2ca8dede66373d6d64ca6f25f5bd2d0cc3ec
furkan83 <118264064+furkan83@users.noreply.github.com>
2023-10-22 20:40:50 +0300

    Update no1.java

--- no1.java
+++ no1.java
@@ -1,10 +1,15 @@
 public class HideAndSeek {
   public static void main(String[] args) {
+    int age;
+    age = 19;
+    String Name = "Furkan" ;
     System.out.println("Let's play hide and seek.");
     System.out.print("Three...");
     System.out.print("Two...");
     System.out.println("One...");
     System.out.println("Ready or not, here I come!");
+
+    System.out.println(name);
 //This is a comment.
 /*This is another way to comment.
 This method is used for multiple lines.*/
